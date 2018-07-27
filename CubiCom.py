bee00d018ba3b4784046dba64fcdd9433f5fdebe
ugtan <umang.taneja0173@gmail.com>
2018-07-27 19:40:40 +0530

    Added Command line arguments

--- CubiCom.py
+++ CubiCom.py
@@ -6,8 +6,10 @@ DEPENDENCIES: 1. requests
               4. termcolor
 """
 
-
+import getpass
+import sys
 import requests
+
 from bs4 import BeautifulSoup
 from tabulate import tabulate
 from termcolor import colored
@@ -28,26 +30,26 @@ def location():
     return city
 
 
-def competitions(soup):
+def getCompetitions(soup):
     """ To Find the cubing competitions"""
+    data = []
     header = ["DATE", "NAME", "VENUE", "LOCATION"]
     try:
 
-        uls = soup.find_all("ul", {"class": "list-group"})
-        for ul in uls:
-            data = []
-            print(ul.find("li").text)
-            lis = ul.find_all("li")[1:20]
-            for li in lis:
-                date = li.find("span", {"class": "date"}).text
-                name = li.find("div", {"class": "competition-link"}).text
-                venue = li.find("div", {"class": "venue-link"}).text
-                location = li.find("div", {"class": "location"}).text
+        ul = soup.find("ul", {"class": "list-group"})
 
-                data.append([date, name, venue, location])
+        print(ul.find("li").text)
+        lis = ul.find_all("li")[1:20]
+        for li in lis:
+            date = li.find("span", {"class": "date"}).text
+            name = li.find("div", {"class": "competition-link"}).text
+            venue = li.find("div", {"class": "venue-link"}).text
+            location = li.find("div", {"class": "location"}).text
 
-            print(colored(tabulate(data, headers=header,
-                                   tablefmt='fancy_grid'), "green"))
+            data.append([date, name, venue, location])
+
+        print(colored(tabulate(data, headers=header,
+                               tablefmt='fancy_grid'), "green"))
     except AttributeError:
 
         print(colored("\nSorry Bad luck no competitions in your area!",
@@ -55,32 +57,46 @@ def competitions(soup):
 
 
 def main():
-    print(colored("\nHey Welcome!", "green", attrs=['blink']))
-    print("\n1. Display all the Competitions in progress as well as all the Upcoming Competitions.\
-           \n2. Find Competitions around your area. \
-           \n3. Find competitions by entering the city.\n")
+    print(colored("\nHey Welcome!\n", "green", attrs=['blink']))
+    args = sys.argv
+    args = args[1:]
 
-    choice = input("Enter your choice : ")
+    if not args:
+        print("\nUsage: CubiCom.py COMMAND [ARGS]...\n\n Get Cubing competitions in the command line!\n\nCommands:\n all     Get all the upcoming competitions around the world.\n live    Get all the cubing competions that are in progress.\n nearme    Get all the cubing competions around me.\n [-c] city    Get all the cubing competions that will be held in the city.")
+        sys.exit()
 
-    while choice not in map(str, range(1, 4)):
-        choice = input(
-            colored("\nYou Entered wrong choice! Enter a valid choice.", "red"))
-    if choice == "1":
-        url = 'https://www.worldcubeassociation.org/competitions'
+    if len(args) < 2:
+        if args[0] == "all":
+            url = 'https://www.worldcubeassociation.org/competitions'
+            response = res(url)
+            soup = BeautifulSoup(response.text, "html.parser")
+            comps = soup.find("div", {"id": "upcoming-comps"})
+            getCompetitions(comps)
 
-    elif choice == "2":
-        city = location()
+        elif args[0] == "live":
+            url = 'https://www.worldcubeassociation.org/competitions'
+            response = res(url)
+            soup = BeautifulSoup(response.text, "html.parser")
+            comps = soup.find("div", {"id": "in-progress-comps"})
+            getCompetitions(comps)
+
+        elif args[0] == "nearme":
+            city = location()
+            url = 'https://www.worldcubeassociation.org/competitions?utf8=%E2%9C%93&region=all&search={}&state=present&year=all+years&from_date=&to_date=&delegate=&display=list'.format(
+                city)
+            response = res(url)
+            comps = BeautifulSoup(response.text, "html.parser")
+            getCompetitions(comps)
+
+        else:
+            print("Sorry, You have entered a wrong command!")
+
+    if len(args) < 3 and args[0] == "-c":
         url = 'https://www.worldcubeassociation.org/competitions?utf8=%E2%9C%93&region=all&search={}&state=present&year=all+years&from_date=&to_date=&delegate=&display=list'.format(
-            city)
-
-    else:
-        city = input("\nEnter the City! ")
-        url = 'https://www.worldcubeassociation.org/competitions?utf8=%E2%9C%93&region=all&search={}&state=present&year=all+years&from_date=&to_date=&delegate=&display=list'.format(
-            city)
-
-    response = res(url)
-    soup = BeautifulSoup(response.text, "html.parser")
-    competitions(soup)
+            args[1])
+        response = res(url)
+        comps = BeautifulSoup(response.text, "html.parser")
+        getCompetitions(comps)
 
 
 if __name__ == '__main__':
