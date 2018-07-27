1e1a566df4e17e218b6e2ce30d3d1eed0d02037b
ugtan <umang.taneja0173@gmail.com>
2018-07-27 18:09:23 +0530

    Fixed Code with PEP8

--- CubiCom.py
+++ CubiCom.py
@@ -14,15 +14,13 @@ from termcolor import colored
 
 
 def res(url):
-    """ To ping the website using the url for information"""
-
+    """To ping the website using the url for information"""
     response = requests.get(url)
     return response
 
 
 def location():
-    """ To find the location of the current user using his ip address"""
-
+    """To find the location of the current user using his ip address"""
     url = 'https://ipinfo.io/'
     response = res(url)
     data = response.json()
@@ -32,54 +30,56 @@ def location():
 
 def competitions(soup):
     """ To Find the cubing competitions"""
-
-
     header = ["DATE", "NAME", "VENUE", "LOCATION"]
     try:
 
-        uls = soup.find_all("ul", {"class":"list-group"})
+        uls = soup.find_all("ul", {"class": "list-group"})
         for ul in uls:
             data = []
             print(ul.find("li").text)
             lis = ul.find_all("li")[1:20]
             for li in lis:
-                date = li.find("span", {"class":"date"}).text
-                name = li.find("div",{"class":"competition-link"}).text
-                venue = li.find("div",{"class":"venue-link"}).text
-                location = li.find("div",{"class":"location"}).text
+                date = li.find("span", {"class": "date"}).text
+                name = li.find("div", {"class": "competition-link"}).text
+                venue = li.find("div", {"class": "venue-link"}).text
+                location = li.find("div", {"class": "location"}).text
 
                 data.append([date, name, venue, location])
 
-            print(colored(tabulate(data, headers = header, tablefmt = 'fancy_grid'), "green"))
+            print(colored(tabulate(data, headers=header,
+                                   tablefmt='fancy_grid'), "green"))
     except AttributeError:
 
-        print(colored("\nSorry Bad luck no competitions in your area!", "red", attrs=['blink']))
+        print(colored("\nSorry Bad luck no competitions in your area!",
+                      "red", attrs=['blink']))
 
 
 def main():
-
     print(colored("\nHey Welcome!", "green", attrs=['blink']))
     print("\n1. Display all the Competitions in progress as well as all the Upcoming Competitions.\
            \n2. Find Competitions around your area. \
            \n3. Find competitions by entering the city.\n")
 
     choice = input("Enter your choice : ")
-    while choice not in map(str,range(1,4)):
-        choice = input(colored("\nYou Entered wrong choice! Enter a valid choice.", "red"))
 
+    while choice not in map(str, range(1, 4)):
+        choice = input(
+            colored("\nYou Entered wrong choice! Enter a valid choice.", "red"))
     if choice == "1":
         url = 'https://www.worldcubeassociation.org/competitions'
 
     elif choice == "2":
         city = location()
-        url = 'https://www.worldcubeassociation.org/competitions?utf8=%E2%9C%93&region=all&search={}&state=present&year=all+years&from_date=&to_date=&delegate=&display=list'.format(city)
+        url = 'https://www.worldcubeassociation.org/competitions?utf8=%E2%9C%93&region=all&search={}&state=present&year=all+years&from_date=&to_date=&delegate=&display=list'.format(
+            city)
 
     else:
-        city =input("\nEnter the City! ")
-        url = 'https://www.worldcubeassociation.org/competitions?utf8=%E2%9C%93&region=all&search={}&state=present&year=all+years&from_date=&to_date=&delegate=&display=list'.format(city)
+        city = input("\nEnter the City! ")
+        url = 'https://www.worldcubeassociation.org/competitions?utf8=%E2%9C%93&region=all&search={}&state=present&year=all+years&from_date=&to_date=&delegate=&display=list'.format(
+            city)
 
     response = res(url)
-    soup = BeautifulSoup(response.text , "html.parser")
+    soup = BeautifulSoup(response.text, "html.parser")
     competitions(soup)
 
 
